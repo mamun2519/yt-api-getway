8d182b19f632a9746fc9136de8b018b43797aa96
mamunMonster <mamun19b3@gmail.com>
2024-02-19 21:01:15 +0600

    complete error handeling

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Application, NextFunction, Request, Response } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { StatusCodes } from "http-status-codes";
+import globalErrorHandler from "./app/middleware/globalErrorHandler";
 const app: Application = express();
 
 //middleware
@@ -23,6 +24,8 @@ app.get("/health", (req: Request, res: Response) => {
 app.get("/", (req: Request, res: Response) => {
   res.status(StatusCodes.OK).send({ success: true, message: "Server is run" });
 });
+// Global Error Handler
+app.use(globalErrorHandler);
 // Not Found Route
 app.use((req: Request, res: Response, next: NextFunction) => {
   res.status(300).json({
