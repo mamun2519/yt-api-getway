30d73a88bbf433e60cf8ede9de7f5aa7d4e2ff21
mamunMonster <mamun19b3@gmail.com>
2024-02-19 21:07:33 +0600

    error handeling

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,39 @@
 import app from "./app";
 import config from "./config";
+import { Server } from "http";
 
 // Main Server
 const bootstrap = async () => {
   try {
-    app.listen(config.port, () => {
+    const server: Server = app.listen(config.port, () => {
       console.log(
-        `API Geteway server Is Run Successfully PORT NO- ${config.port}`
+        `API Getaway server Is Run Successfully PORT NO- ${config.port}`
       );
+    });
+    const exitHandler = () => {
+      if (server) {
+        server.close(() => {
+          console.log("Server closed");
+          process.exit(1);
+        });
+      } else {
+        process.exit(1);
+      }
+    };
+
+    const unexpectedErrorHandler = (error: unknown) => {
+      console.log(error);
+      exitHandler();
+    };
+
+    process.on("uncaughtException", unexpectedErrorHandler);
+    process.on("unhandledRejection", unexpectedErrorHandler);
+
+    process.on("SIGTERM", () => {
+      console.log("SIGTERM received");
+      if (server) {
+        server.close();
+      }
     });
   } catch (error) {
     console.log(error);
