95d051633dc294a12a3ba5ccb0f64c7d4d259595
mamunMonster <mamun19b3@gmail.com>
2024-02-27 12:26:38 +0600

    complete get by user emmail

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { UserController } from "./user.controller";
-
+//
 const router = express.Router();
 router.get("/:email", UserController.getUserByEmail);
 router.get("/all-user", UserController.getAllUser);
